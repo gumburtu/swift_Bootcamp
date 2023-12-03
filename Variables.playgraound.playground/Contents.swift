c15afe1a5b56cd6364ba7846213519fb6292c2d5
Seval <aslanseval1986@gmail.com>
2023-12-03 12:56:41 +0000

    Add files via upload

---
+++ Variables.playgraound.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+let a = 14
+var b = 15
+
+var ogrenciAdi = "Seval"
+var ogrenciBoy = 1.83
+var ogrenciYas = 37
+var ogrenciHalaOkuyorMu = true
+
+var ilce = "Luleburgaz"
+print("Ilce : \(ilce)")
+
+var InstagramAdress = "@gumburtu"
+print("Instagram Adresi : \(InstagramAdress)")
+
+var linkedinmAdress = "https://www.linkedin.com/in/sevalaslan"
+print("Linkedin Adresi : \(linkedinmAdress)")
+
+
+print(ogrenciAdi)
+print(ogrenciBoy)
+print(ogrenciYas)
+print(ogrenciHalaOkuyorMu)
+
+print("Ogrenci Adi                    \(ogrenciAdi)")
+print("Ogrenci Boy:                   \(ogrenciBoy)")
+print("Ogrenci Yas:                   \(ogrenciYas)")
+print("Ogrenci Okula Devam Ediyor mu: \(ogrenciHalaOkuyorMu)")
+
+var c = a + b
+
+let x : String = "Seval Aslan"
+
+print(c)
+print(a+b)
+
+print("Merhaba Dunya!!!")
+
+if (c > 30)
+{
+    print("Sayiniz 29'dan buyuktur!")
+}
+    else
+    {
+        print("Sayiniz 29'dan kucuktur!")
+    }
+
+print(x)
